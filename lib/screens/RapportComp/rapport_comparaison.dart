5e3e9a8eaf3981ef3405e78cbf21c5aeff0c0530
j4niro <junirobini44@gmail.com>
2024-07-18 11:37:12 +0200

    rapport de consommation

--- lib/screens/RapportComp/rapport_comparaison.dart
+++ lib/screens/RapportComp/rapport_comparaison.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
+import 'package:mobile_kania_flutter/components/botton_navbar.dart';
 import 'package:mobile_kania_flutter/components/custom_app_bar.dart';
 
 class RapportComparaison extends StatefulWidget {
@@ -18,11 +19,21 @@ class _RapportComparaisonState extends State<RapportComparaison> {
     return Scaffold(
       appBar: CustomAppBar(),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(10.0),
         child: Container(
           //width: width*0.8,
           height: height*0.8,
-          color: Colors.yellow,
+          decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.25),
+              spreadRadius: 0,
+              blurRadius: 4,
+              offset: const Offset(0, 4),
+            ),
+          ],
+          ),
           child: Column(
             children: [
               SizedBox(height: 15,),
@@ -48,9 +59,191 @@ class _RapportComparaisonState extends State<RapportComparaison> {
               SizedBox(height: 20,),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
-                children: [Text("Date d'émission :" + DateFormat.DAY)],)
+                children: [Text("Date d'émission :" + '16/07/2024')],),
+              SizedBox(height: 30,),
+              Column(
+                children: [
+                  Center(child: Text('Comparaison de consommation du', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)),
+                  Center(child: Text('17/07/2023-18/07/2024',style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color(0xffff7900)),)),
+                ],
+              ),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text('Facture fourni par IHS'),
+                ],
+              ),
+              Container(
+                //width: width*0.95,
+                //height: height*0.3,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color:Colors.black.withOpacity(0.25),
+                      spreadRadius: 0,
+                      blurRadius: 3.84,
+                      offset: const Offset(0, 2), 
+                    ),
+                  ],
+                ),
+                child: Table(
+                  //width : width*0.8,
+                  border: TableBorder(
+                    top: BorderSide.none,
+                    bottom: BorderSide.none,
+                    ),
+                  children: [
+                    TableRow(
+                      decoration: BoxDecoration(
+                        color: Colors.black
+                        ),
+                      
+                      children: [
+                        Column(children:[Text('TITRE', style: TextStyle(color: Colors.white),)]),
+                        Column(children:[Text('VALEURS',style: TextStyle(color: Colors.white)), ]),
+                      ]
+                    ),
+                    TableRow(
+                      children: [
+                        Column(children:[Text('Consommation en kWh')]),
+                        Column(children:[Text('5000')]),
+                      ]
+                    ),
+                    TableRow(
+                      children: [
+                        Column(children:[Text('Facture en FCFA')]),
+                        Column(children:[Text('350000')]),
+                      ]
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text('Rapport de KANIA'),
+                ],
+              ),
+              Container(
+                //width: width*0.95,
+                //height: height*0.3,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color:Colors.black.withOpacity(0.25),
+                      spreadRadius: 0,
+                      blurRadius: 3.84,
+                      offset: const Offset(0, 2), 
+                    ),
+                  ],
+                ),
+                child: Table(
+                  //width : width*0.8,
+                  border: TableBorder(
+                    top: BorderSide.none,
+                    bottom: BorderSide.none,
+                    ),
+                  children: [
+                    TableRow(
+                      decoration: BoxDecoration(
+                        color: Colors.black
+                        ),
+                      
+                      children: [
+                        Column(children:[Text('TITRE', style: TextStyle(color: Colors.white),)]),
+                        Column(children:[Text('VALEURS',style: TextStyle(color: Colors.white)), ]),
+                      ]
+                    ),
+                    TableRow(
+                      children: [
+                        Column(children:[Text('Consommation en kWh')]),
+                        Column(children:[Text('4300')]),
+                      ]
+                    ),
+                    TableRow(
+                      children: [
+                        Column(children:[Text('Facture en FCFA')]),
+                        Column(children:[Text('310000')]),
+                      ]
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 30,),
+              Container(
+                //width: width*0.95,
+                //height: height*0.3,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color:Colors.black.withOpacity(0.25),
+                      spreadRadius: 0,
+                      blurRadius: 3.84,
+                      offset: const Offset(0, 2), 
+                    ),
+                  ],
+                ),
+                child: Table(
+                  columnWidths: const {
+                    0: FlexColumnWidth(3),
+                    1: FlexColumnWidth(1),
+                    2: FlexColumnWidth(1),
+                    3: FlexColumnWidth(2),
+                  },
+                  //width : width*0.8,
+                  border: TableBorder(
+                    top: BorderSide.none,
+                    bottom: BorderSide.none,
+                    ),
+                  children: [
+                    TableRow(
+                      decoration: BoxDecoration(
+                        color: Colors.black
+                        ),
+                      
+                      children: [
+                        Column(children:[Text('TITRE', style: TextStyle(color: Colors.white),)]),
+                        Column(children:[Text('IHS',style: TextStyle(color: Colors.white)), ]),
+                        Column(children:[Text('KANIA',style: TextStyle(color: Colors.white)), ]),
+                        Column(children:[Text('DIFFERENCE',style: TextStyle(color: Colors.white)), ]),
+                      ]
+                    ),
+                    TableRow(
+                      children: [
+                        Column(children:[Text('Consommation en kWh')]),
+                        Column(children:[Text('4300')]),
+                        Column(children:[Text('5000')]),
+                        Column(children:[Text('+700', style: TextStyle(backgroundColor: Colors.red, color: Colors.white))]),
+                      ]
+                    ),
+                    TableRow(
+                      children: [
+                        Column(children:[Text('Facture en FCFA')]),
+                        Column(children:[Text('310000')]),
+                        Column(children:[Text('350000')]),
+                        Column(children:[Text('+40000', style: TextStyle(backgroundColor: Colors.red, color: Colors.white) ,)]),
+                      ]
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text('Conclusion'),
+                ],
+              )
             ],
         ),
-      )));
+      )),
+      bottomNavigationBar: BottomNavbarWidget(),
+      );
   }
 }
